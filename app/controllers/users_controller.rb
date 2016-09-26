6e1e9ad279aa77b0de630fc8f78b239a0d232735
Rusty Hutto <email": null>
2016-09-25 21:02:08 -0700

    Add login function, and user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,31 @@
 # new page for creating a user
 get '/users/new' do
-
   erb :'/users/new'
 end
 
 # create a new user
 post '/users' do
-
+  @user = User.new(params[:user])
+  if @user.save
+    session[:user] = @user
+    redirect "/users/#{@user.id}"
+  else
+    @errors = ["Username or Email already exists"]
+    erb :'users/new'
+  end
 end
 
 # show the signed in user
 get '/users/:id' do
   @user = User.find(params[:id])
+  redirect '/sessions/new' unless current_user
+  redirect '/' unless logged_in?
   erb :'/users/show'
 end
 
 # get to the page to edit user information
 get '/users/:id/edit' do
-
+  @user = User.find(params[:id])
   erb :'/users/edit'
 end
 

466733a23d97ee81cd13b6989b67236c08c7e30a
Rusty Hutto <email": null>
2016-09-26 16:40:49 -0700

    Start controller for sightings

---
+++ app/controllers/sightings_controller.rb
@@ -0,0 +1,23 @@
+get '/sightings/new' do
+  erb :'/sightings/new'
+end
+
+post '/sightings' do
+
+end
+
+get '/sightings/:id' do
+
+end
+
+get '/sightings/:id/edit' do
+
+end
+
+put '/sightings/:id' do
+
+end
+
+delete '/sightings/:id' do
+
+end

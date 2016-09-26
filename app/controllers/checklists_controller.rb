fe52a00f22531d7b3f56cf46566e700c93f922a9
Rusty Hutto <email": null>
2016-09-25 21:47:21 -0700

    Add a new checklist page

---
+++ app/controllers/checklists_controller.rb
@@ -0,0 +1,19 @@
+get '/checklists/new' do
+  erb :'/checklists/new'
+end
+
+post '/checklists' do
+
+end
+
+get '/checklists/:id' do
+  erb :'/checklists/show'
+end
+
+put '/checklists/:id/edit' do
+
+end
+
+delete '/checklists/:id' do
+
+end

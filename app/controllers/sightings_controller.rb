71a4a957421863af5a64166dcb73d814c7e5cbba
Rusty Hutto <email": null>
2016-09-26 19:07:30 -0700

    Add sightings view, and pages

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,17 +1,28 @@
-get '/sightings/new' do
+get '/checklists/:checklist_id/sightings/new' do
+  @checklist = Checklist.find(params[:checklist_id])
   erb :'/sightings/new'
 end
 
-post '/sightings' do
-
+post '/checklists/:checklist_id/sightings' do
+  @checklist = Checklist.find(params[:checklist_id])
+  @sighting = @checklist.sightings.build(params[:sighting])
+  if @checklist.save
+    redirect "/checklists/#{@checklist.id}"
+  else
+    @errors = ["One or more fields are invalid"]
+    erb :'sightings/new'
+  end
 end
 
-get '/sightings/:id' do
-
+get '/checklists/:checklist_id/sightings/:id' do
+  @checklist = Checklist.find(params[:checklist_id])
+  @sighting = Sighting.find(params[:id])
+  erb :'/sightings/show'
 end
 
 get '/sightings/:id/edit' do
-
+  @sighting = Sighting.find(params[:id])
+  erb :'/sightings/edit'
 end
 
 put '/sightings/:id' do

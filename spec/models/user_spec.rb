650010c1059011f4c80aa748e3ec999c9ba22b3e
Rusty Hutto <email": null>
2016-09-29 16:57:40 -0700

    Add rspec tests for user assocations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,34 +2,30 @@ require_relative "../spec_helper"
 
 describe User do
   describe 'associations' do
-    # before(:each) do
-    #   user = User.create(username: "Rusty",
-    #                        email: "fake@fake.com",
-    #                        password: "123")
+    before(:each) do
+      @user = User.first
 
-    #   yosemite = Checklist.create(list_name: "Yosemite",
-    #                                 location: "Yosemite National Park",
-    #                                 datetime: "2001-02-03T00:00:00+00:00",
-    #                                 user_id: user.id)
+      @yosemite = @user.checklists.create!(list_name: "Yosemite1",
+                                    location: "Yosemite National Park",
+                                    datetime: "2001-02-03T00:00:00+00:00")
 
-    #   chickadee = Sighting.create(species: "Black-capped Chickadee",
-    #                                 count: 2,
-    #                                 distance: 120,
-    #                                 observation_type: "v",
-    #                                 location: "Near half dome",
-    #                                 habitat: "Conifer Forest",
-    #                                 vegetation: "Ponderosa Pine",
-    #                                 checklist_id: yosemite.id,
-    #                                 user_id: user.id)
+      @chickadee = @yosemite.sightings.create!(species: "Black-capped Chickadee",
+                                    count: 2,
+                                    distance: 120,
+                                    observation_type: "v",
+                                    location: "Near half dome",
+                                    habitat: "Conifer Forest",
+                                    vegetation: "Ponderosa Pine")
 
-    # end
+    end
 
     it "has a checklist" do
-      user = User.new(username: "Rusty",
-                           email: "fake@fake.com",
-                           password: "123")
-
-      expect(user.checklists).to match_array [yosemite]
+      expect(@user.checklists).to include(@yosemite)
     end
+
+    it "has a sighting" do
+      expect(@user.sightings).to include(@chickadee)
+    end
+
   end
 end

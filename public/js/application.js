6e90e0bd37e2cd15a171f9e33e3e5c848299c3b3
Rusty Hutto <email": null>
2016-09-30 11:59:06 -0700

    Add lots of styling

--- public/js/application.js
+++ public/js/application.js
@@ -65,5 +65,6 @@ function geocodeAddress(geocoder, resultsMap) {
     } else {
       alert('Geocode was not successful for the following reason: ' + status);
     }
+  console.log(results[0].geometry.location)
   });
 }

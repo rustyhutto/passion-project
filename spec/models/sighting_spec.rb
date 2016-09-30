fc8813048dfe2a7e384b5b0ad313f093425aadbd
Rusty Hutto <email": null>
2016-09-30 12:34:47 -0700

    Adding more tests

--- spec/models/sighting_spec.rb
+++ spec/models/sighting_spec.rb
@@ -1,1 +1,14 @@
+require_relative "../spec_helper"
 
+describe Sighting do
+  describe 'attributes' do
+    let(:sighting) (Sighting.create(species: "Black-capped Chickadee",
+                                    count: 2,
+                                    distance: 120,
+                                    observation_type: "v",
+                                    location: "Near half dome",
+                                    habitat: "Conifer Forest",
+                                    vegetation: "Ponderosa Pine"))
+    # expect
+  end
+end
